4ae07358ce32729ae20701004b206b32d8265197
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-03-07 20:11:12 +0100

    added sql file that install databases and tables for spring-security and jdbc authentication

---
+++ sql/spring-security-db-init/install-spring-security.sql
@@ -0,0 +1,28 @@
+
+BEGIN TRAN
+
+BEGIN TRY
+
+	CREATE TABLE users (
+  		username VARCHAR(50) NOT NULL,
+  		password VARCHAR(100) NOT NULL,
+  		enabled TINYINT NOT NULL DEFAULT 1,
+  		PRIMARY KEY (username)
+	);
+  
+	CREATE TABLE authorities (
+  		username VARCHAR(50) NOT NULL,
+  		authority VARCHAR(50) NOT NULL,
+  		FOREIGN KEY (username) REFERENCES users(username)
+	);
+
+	CREATE UNIQUE INDEX ix_auth_username
+  		on authorities (username,authority);
+
+ 	 COMMIT;
+ END TRY
+ 
+ BEGIN CATCH
+	ROLLBACK TRAN;
+ END CATCH
+
