afa0f3a4840afe3a930997e24467ec2928d6f975
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-03-07 19:31:25 +0100

    added ant matchers with mutlipath for root

--- src/main/java/com/example/dummy/configuration/ApplicationSecurityConfig.java
+++ src/main/java/com/example/dummy/configuration/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,8 +19,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure( AuthenticationManagerBuilder auth) throws Exception
     {
-        auth.inMemoryAuthentication().withUser("root").password(this.getPasswordEncoder().encode("root")).roles("ROOT");
+        auth.inMemoryAuthentication().withUser("root").roles("ADMIN").password(this.getPasswordEncoder().encode("root"));
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests().
+                antMatchers("/home").permitAll()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/user").hasRole("ADMIN")
+                .antMatchers("/admin").hasRole("ADMIN").and().formLogin();
     }
 
     @Bean
--- src/main/java/com/example/dummy/controller/SecurityExampleController.java
+++ src/main/java/com/example/dummy/controller/SecurityExampleController.java
@@ -1,4 +1,31 @@
 package com.example.dummy.controller;
 
-public class SecurityExampleController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityExampleController
+{
+
+    @GetMapping("/")
+    public String home()
+    {
+        return "welcome to home";
+    }
+
+    @GetMapping("/admin")
+    public String admin()
+    {
+        return "admin page";
+    }
+
+    @GetMapping("/user")
+    public String user()
+    {
+        return "user page";
+    }
+
+
+
+
 }
