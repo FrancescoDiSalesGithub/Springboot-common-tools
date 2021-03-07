bdb318dcc7c3c4a974ccd2c2e1322774772b20aa
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-03-07 19:54:01 +0100

    jdbc authentication problem with mssql

--- src/main/java/com/example/dummy/configuration/ApplicationSecurityConfig.java
+++ src/main/java/com/example/dummy/configuration/ApplicationSecurityConfig.java
@@ -1,26 +1,51 @@
 package com.example.dummy.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.core.userdetails.User;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
 {
+    @Autowired
+    DataSource dataSource;
+
 
     @Override
     protected void configure( AuthenticationManagerBuilder auth) throws Exception
     {
-        auth.inMemoryAuthentication().withUser("root").roles("ADMIN").password(this.getPasswordEncoder().encode("root"));
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("root")
+                        .password("root")
+                        .roles("ADMIN")
+                        )
+                .withUser(
+                        User.withUsername("user")
+                        .password("user")
+                        .roles("USER")
+                );
 
+        auth.inMemoryAuthentication().withUser("root").roles("ADMIN").password(this.getPasswordEncoder().encode("root"));
+        auth.inMemoryAuthentication().withUser("user").roles("USER").password(this.getPasswordEncoder().encode("user"));
     }
 
     @Override
@@ -35,8 +60,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
     @Bean
     public PasswordEncoder getPasswordEncoder()
     {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
     }
-
 
 }
